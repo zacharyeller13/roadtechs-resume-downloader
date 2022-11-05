17b79640727102c71b50a7685aad1f85ed844648
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2022-11-05 11:52:56 -0500

    Update authentication tests

--- roadtechs_downloader.py
+++ roadtechs_downloader.py
@@ -1,10 +1,32 @@
 from bs4 import BeautifulSoup, Comment
-from requests import Session
+from requests import Session, RequestException
 from exceptions import AlreadyLoggedInError
 
-def authenticate(username: str, password: str) -> Session:
+def authenticate(url: str, username: str, password: str) -> Session:
+    """
+    Return persistant, logged in Session object for use in requests
+    """
 
-    raise AlreadyLoggedInError
+    session = Session()
+
+    resp = session.post(url, data={
+        "client_name": username,
+        "pass1": password,
+        "Login": "Login"
+    })
+
+    if resp.status_code == 200:
+
+        soup = BeautifulSoup(resp.content, "html.parser")
+
+        if soup.find_all("input", {"name": "login2", "value": "Force Login"}):
+
+            raise AlreadyLoggedInError
+
+        return session
+        
+    else:
+        raise RequestException(f"Request returned {resp.status_code}")
 
 def get_printable_profile() -> BeautifulSoup:
 
--- tests/test_authenticate.py
+++ tests/test_authenticate.py
@@ -1,8 +1,14 @@
 from exceptions import AlreadyLoggedInError
+from requests import RequestException
 from roadtechs_downloader import authenticate
 import pytest
 
-def test_authenticate() -> None:
+def test_authenticate_logged_in() -> None:
 
     with pytest.raises(AlreadyLoggedInError):
-        authenticate("test", "test")
+        authenticate("https://www.roadtechs.com/bbclient/login.php", "", "")
+
+def test_authenticate_bad_request() -> None:
+
+    with pytest.raises(RequestException):
+        authenticate("https://www.roadtechs.com/bbclient/loginurl.php", "test", "test")
