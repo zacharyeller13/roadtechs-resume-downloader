97a5f4f791b41f573a52a7662d672ec19ec04ec3
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2022-12-03 16:02:39 -0600

    Add function docstrings

--- async_roadtechs_downloader.py
+++ async_roadtechs_downloader.py
@@ -8,20 +8,27 @@ from exceptions import AlreadyLoggedInError, LoginError
 
 
 def parse_login(soup: BeautifulSoup) -> None:
+    """
+    Parse HTML of login attempt to raise errors if login fails
+
+    Raises an error or returns None
+    """
+
     def find_error(tag):
         return tag.name=='b' and "Error Logging In:" in tag.parent.contents[0]
 
     if soup.find_all("input", {"name": "login2", "value": "Force Login"}):
         raise AlreadyLoggedInError("The user is already logged in elsewhere - please close all sessions")
+        
     elif soup.find_all(find_error):
         raise LoginError("Username or password was incorrect. Please retry.")
 
 async def authenticate(session: ClientSession, url: str, username: str, password: str) -> ClientResponse:
     """
-    Log into a ClientSession for use in async requests,
+    Log into a `ClientSession` for use in async requests,
     raising exception for response other than 200 or if user already logged in
 
-    Return a ClientResponse object
+    Return a `ClientResponse` object
     """
 
     resp = await session.post(url, data={
@@ -39,16 +46,17 @@ async def authenticate(session: ClientSession, url: str, username: str, password
     return resp
 
 async def deauth(session: ClientSession) -> ClientResponse:
+    """
+    Log out of `ClientSession`
+    """
 
-    resp = await session.post("https://www.roadtechs.com/bbclient/logout.php")
-
-    return resp
+    return await session.post("https://www.roadtechs.com/bbclient/logout.php")
 
 def get_tasks(url: str, session: ClientSession) -> list[asyncio.Task]:
     """
     Get all async tasks for requesting printable profiles
 
-    Return a list of asyncio.Task objects
+    Return a list of `asyncio.Task` objects
     """
 
     tasks = []
