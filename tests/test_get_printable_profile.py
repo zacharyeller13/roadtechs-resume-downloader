32143599359395a99c23b10bbe28972e82d6363f
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2022-12-03 16:07:54 -0600

    Delete old tests and files

--- tests/test_get_printable_profile.py
+++
@@ -1,7 +0,0 @@
-from roadtechs_downloader import get_printable_profile
-import pytest
-
-def test_get_printable_profile() -> None:
-
-    with pytest.raises(NotImplementedError):
-        get_printable_profile()
