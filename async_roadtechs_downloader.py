6f77e937eb612b408c1f7d5fa35dab51b6948428
zacharyeller13 <37415056+zacharyeller13@users.noreply.github.com>
2023-01-25 22:09:00 -0600

    Add initial resume writing function to main

--- async_roadtechs_downloader.py
+++ async_roadtechs_downloader.py
@@ -5,7 +5,7 @@ from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 
 from aiohttp_extensions import authenticate, deauth
-from pdf_writer import write_pdf, get_resume_name
+from pdf_writer import write_pdf
 from tasks import get_profile_tasks, get_validation_tasks
 
 
@@ -69,7 +69,10 @@ async def main() -> None:
 
         # Need to write only valid resumes to PDF files
         # Do this for the initial set of responses, then again below for each request-validation loop
-        # TODO
+        for response, is_valid in zip(responses, validations):
+
+            if is_valid:
+                write_pdf(BeautifulSoup(await response.text(), "html.parser"))
 
         # All other validations
         while valid_count != resume_count:
